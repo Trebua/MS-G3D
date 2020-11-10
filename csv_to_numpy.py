616426d6fbf40affe54c4e6a351b12bf9c9fe220
Andreas Haukeland <andreas.haukeland.95@gmail.com>
2020-11-10 23:45:52 +0100

    iterate

--- csv_to_numpy.py
+++ csv_to_numpy.py
@@ -1,8 +1,10 @@
 import os
 import numpy as np
 
+# Script to convert the CIMA csv files into the appropriate npy and json format to use the gen_data from MS-G3D
+# The label files looks like: (["file_names"], ["values"])
 
-files = os.listdir("./coordinates")
+files = os.listdir("./coordinates")  # This file should be placed under /dataset/cima/raw/
 
 # shape = (N, DIM, T, V, M))
 N = len(files)
@@ -10,37 +12,41 @@ DIM = 2  # Dimensions of coordinates
 T = 300  # How long the samples should be
 V = 19  # Features
 M = 1  # Number of people
+X_index, Y_index = 0, 1
+
 sample_arr = np.zeros((N, DIM, T, V, M))
 
-for i, file_name in enumerate(files):
-    with open(file_name) as csv_file:
+for n, file_name in enumerate(files):
+    with open("./coordinates/" + file_name) as csv_file:
         header = csv_file.readline().split(",")
 
-        x_arr = np.zeros((4, 19, 1))
-        y_arr = np.zeros((4, 19, 1))
-
+        x_arr = np.zeros((T, V, M))
+        y_arr = np.zeros((T, V, M))
         for line in csv_file:
             frame_data = line.split(",")
-            frame = int(frame_data[0]) - 1
-            if frame >= 300:
+            try:
+                t = int(frame_data[0]) - 1
+            except ValueError:
+                continue
+
+            if t >= T:
                 break
 
             mat_x = np.array([])
             mat_y = np.array([])
-            for i in range(1, len(frame_data)):
-                val = float(frame_data[i])
-                if i % 2 == 0:  # Add to Y
+            for j in range(1, len(frame_data)):
+                val = float(frame_data[j])
+                if j % 2 == 0:  # Add to Y
                     mat_y = np.append(mat_y, float(val))
                 else:
                     mat_x = np.append(mat_x, float(val))
 
-            mat_x = mat_x.reshape([19, 1])
-            mat_y = mat_y.reshape([19, 1])
+            mat_x = mat_x.reshape([V, M])
+            mat_y = mat_y.reshape([V, M])
 
-            x_arr[frame, :, :] = mat_x
-            y_arr[frame, :, :] = mat_y
-
-        sample_arr[i, 0, :, :, :] = x_arr
-        sample_arr[i, 1, :, :, :] = y_arr
+            x_arr[t, :, :] = mat_x
+            y_arr[t, :, :] = mat_y
+        sample_arr[n, X_index, :, :, :] = x_arr
+        sample_arr[n, Y_index, :, :, :] = y_arr
 
 print(sample_arr)
